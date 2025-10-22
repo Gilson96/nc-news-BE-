2bcc0c57b18f6f883e3694b6c932f38b0d8dda4b
Gilson96 <grafael99@gmail.com>
2025-10-22 13:02:08 +0100

    added GET test for /api/articles/:article_id/comments endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,10 +39,53 @@ describe("GET /api/articles/:article_id", () => {
       .expect(200)
       .then(({ body }) => {
         const { article } = body;
-        console.log(body)
         expect(body).toHaveProperty("article");
         expect(typeof article).toBe("object");
         expect(article).toHaveProperty("article_id", 1);
+      });
+  });
+});
+
+describe("GET /api/articles", () => {
+  it("200 responds with an array containing all topics", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Array.isArray(body)).toBe(true);
+        body.forEach((article) => {
+          expect(article).toHaveProperty("article_id");
+          expect(article).toHaveProperty("title");
+          expect(article).toHaveProperty("topic");
+          expect(article).toHaveProperty("author");
+          expect(article).toHaveProperty("body");
+          expect(article).toHaveProperty("created_at");
+          expect(article).toHaveProperty("votes");
+          expect(article).toHaveProperty("article_img_url");
+          expect(typeof article).toBe("object");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.body).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+        });
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id/comments", () => {
+  it("200 response with the comment object of the given article id", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then(({ body }) => {
+        const { comment } = body;
+        expect(body).toHaveProperty("comment");
+        expect(typeof comment).toBe("object");
+        expect(comment).toHaveProperty("comment_id", 2);
       });
   });
 });
