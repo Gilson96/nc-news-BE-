9ca0913d9c45e7108e4747e37eda372a45a5d9f6
Gilson96 <grafael99@gmail.com>
2025-10-22 13:02:26 +0100

    added GET /api/articles/:article_id/comments endpoint

--- app.js
+++ app.js
@@ -10,12 +10,25 @@ app.get("/api/topics", (req, res) => {
 
 app.get("/api/articles/:article_id", (req, res) => {
   const article_id = req.params.article_id;
-
   return db
     .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
     .then(({ rows }) => {
-      console.log(rows);
       return res.status(200).send({ article: rows[0] });
+    });
+});
+
+app.get("/api/articles", (req, res) => {
+  return db.query("SELECT * FROM articles;").then(({ rows }) => {
+    return res.status(200).send(rows);
+  });
+});
+
+app.get("/api/articles/:article_id/comments", (req, res) => {
+  const article_id = req.params.article_id;
+  return db
+    .query("SELECT * FROM comments WHERE article_id = $1;", [article_id])
+    .then(({ rows }) => {
+      return res.status(200).send({ comment: rows[0] });
     });
 });
 
