05ce99a6fe52beef3e64c30a2c8a3f179dbc615e
Gilson96 <grafael99@gmail.com>
2025-10-27 15:53:13 +0000

    Fixed connection

--- db/connection.js
+++ db/connection.js
@@ -1,22 +1,31 @@
 const { Pool } = require("pg");
+require("dotenv").config({
+  path: `${__dirname}/../.env.${process.env.NODE_ENV || "development"}`,
+});
 
 const ENV = process.env.NODE_ENV || "development";
 
-const config = {};
+let config = {};
+
 
 if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    max: 2,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
 }
 
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-
-const db = new Pool();
+const db = new Pool(config);
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 } else {
-  console.log(`Connected to ${process.env.DATABASE_URL}`);
+  console.log(
+    `Connected to ${process.env.DATABASE_URL || process.env.PGDATABASE}`
+  );
 }
 
 module.exports = db;
