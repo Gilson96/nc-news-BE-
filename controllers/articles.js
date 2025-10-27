cc7be8aad3627aa2cb21952c41ae0ce6db7ebe15
Gilson96 <grafael99@gmail.com>
2025-10-27 12:25:22 +0000

    Added new validation checking

--- controllers/articles.js
+++ controllers/articles.js
@@ -17,12 +17,10 @@ exports.getAllArticles = (req, res) => {
 
 exports.getArticleById = (req, res) => {
   const article_id = req.params.article_id;
- 
+
   return articlesById(article_id).then((article) => {
     if (article.length === 0) {
-      return res
-        .status(404)
-        .send({ msg: `No results for article ${article_id}` });
+      return res.status(404).send({ msg: `No results` });
     } else {
       return res.status(200).send({ article: article[0] });
     }
@@ -35,9 +33,7 @@ exports.editArticle = (req, res) => {
 
   return articleEdit(article_id, inc_votes).then((article) => {
     if (article.length === 0) {
-      return res
-        .status(404)
-        .send({ msg: `No results for article ${article_id}` });
+      return res.status(404).send({ msg: `No results ` });
     } else {
       return res.status(201).send({ article: article[0] });
     }
@@ -45,17 +41,18 @@ exports.editArticle = (req, res) => {
 };
 
 exports.addArticles = (req, res) => {
-  const article_id = req.params.article_id;
   const { body } = req.body;
   const { username } = req.body;
 
+  if (body === undefined || username === undefined) {
+    return res.status(400).send({ msg: `Bad request` });
+  }
+
+  if (typeof body !== "string" || typeof username !== "string") {
+    return res.status(400).send({ msg: `Bad request` });
+  }
+
   return articleAdd(body, username).then((article) => {
-    if (article.length === 0) {
-      return res
-        .status(404)
-        .send({ msg: `No results for article ${article_id}` });
-    } else {
-      return res.status(201).send({ article: article[0] });
-    }
+    return res.status(201).send({ article: article[0] });
   });
 };
