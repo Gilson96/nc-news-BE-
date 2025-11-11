e295e36d52e7d1f97a2582755129a1fd5ee50121
Gilson96 <grafael99@gmail.com>
2025-11-11 09:32:28 +0000

    Fixed articles to allow req.query

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -90,11 +90,11 @@ describe("GET /api/articles", () => {
       order: "DESC",
     };
     return request(app)
-      .get("/api/articles?topic=mitch")
+      .get("/api/articles?sort_by&order&topic=cats")
       .send(sortBy)
       .expect(200)
       .then(({ body }) => {
-        // console.log(body);
+        console.log(body);
       });
   });
   it("should responds with a 200 status code and an array containing all article filtered by topic", () => {
